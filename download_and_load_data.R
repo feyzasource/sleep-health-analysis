90f958168a41792882c1f9b33e5e25aef7dfd794
feyza <feyza@FEYZA-PC>
2024-12-13 13:08:29 +0300

    Updated download script with minor changes

--- download_and_load_data.R
+++ download_and_load_data.R
@@ -1,17 +1,21 @@
+# Script to download data from Kaggle
+# This script downloads datasets using the Kaggle API on Windows.
+# For more information: https://www.kaggle.com/docs/api
 download_and_load <- function(dataset_id, unzip_dir) {
-  # Veri setini indir
+  # Download the dataset
   system(paste0("kaggle datasets download -d ", dataset_id, " -p ", unzip_dir))
   
-  # ZIP dosyasını aç
+  # Extract the ZIP file
   zip_file <- list.files(unzip_dir, pattern = "\\.zip$", full.names = TRUE)
   unzip(zip_file, exdir = unzip_dir)
   
-  # Veri setini oku
+  # Read the dataset
   csv_file <- list.files(unzip_dir, pattern = "\\.csv$", full.names = TRUE)
   data <- read.csv(csv_file)
   return(data)
 }
 
-# Fonksiyonu çağırarak bir veri seti indirin ve okuyun
+# Call the function to download and read a dataset
 data <- download_and_load("owner-name/dataset-name", "./data")
 head(data)
+
